470ad300761d6c8f05a3e3bda2c32fbacd86122e
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-03 00:13:57 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,33 @@
-import express from "express";
-import dotenv from "dotenv";
- import  connectDB  from "./config/database.js";
+import express from "express"; 
+import dotenv from "dotenv"; 
+import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
+import messageRoute from "./routes/messageRoute.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { app,server } from "./socket/socket.js";
 dotenv.config({});
 
-const app = express();
+ 
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json()); 
+app.use(cookieParser());
+const corsOption={
+    origin:'http://localhost:3000',
+    credentials:true
+};
+app.use(cors(corsOption)); 
 
 
-const PORT =   8080 ;
+// routes
+app.use("/api/v1/user",userRoute); 
+app.use("/api/v1/message",messageRoute);
+ 
 
-app.listen(PORT, () => {
-     connectDB();
-    console.log(`Server listening on port ${PORT}`)
-})
+server.listen(PORT, ()=>{
+    connectDB();
+    console.log(`Server listen at prot ${PORT}`);
+});
