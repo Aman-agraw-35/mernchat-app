d037da2f51a8f4084d5584634b3b9255221b25a0
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-16 04:39:22 +0530

    ....

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,6 +1,6 @@
 import { Conversation } from "../models/conversationModel.js";
 import { Message } from "../models/messageModel.js";
-import { getReceiverSocketId, io } from "../api/socket/socket.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req,res) => {
     try {
