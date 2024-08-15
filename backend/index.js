06f623098eef548e6c4b0b1aa40bcafece43c09d
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-16 05:27:46 +0530

    ,,

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOption={
-    origin:'https://mernchat-app-beryl.vercel.app',
+    origin:'https://mernchat-app-git-master-aman-agraw-35s-projects.vercel.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
