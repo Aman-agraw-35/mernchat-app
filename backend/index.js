981f4b5608ac8d4faf8b64d9892cb6b4eaa93c08
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-15 20:48:58 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://mernchat-app-beryl.vercel.app/',
+    origin:'https://mernchat-app-beryl.vercel.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
