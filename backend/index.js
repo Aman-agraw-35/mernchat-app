53b8ecc08df15983123341cc6eab0ea9db37d958
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-15 21:41:49 +0530

    ....

--- backend/index.js
+++ backend/index.js
@@ -5,31 +5,29 @@ import userRoute from "./api/routes/userRoute.js";
 import messageRoute from "./api/routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app,server } from "./api/socket/socket.js";
+import { app, server } from "./api/socket/socket.js";
+
 dotenv.config({});
 
- 
-const PORT = process.env.PORT || 5000;
-
 // middleware
-app.use(express.urlencoded({extended:true}));
-app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cookieParser());
-const corsOption={
-    origin:'https://mernchat-app-beryl.vercel.app',
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-    credentials:true
-};
-app.use(cors(corsOption)); 
 
+const corsOptions = {
+    origin: 'https://mernchat-app-beryl.vercel.app',
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+};
+app.use(cors(corsOptions));
 
 // routes
-app.use("/api/v1/user",userRoute); 
-app.use("/api/v1/message",messageRoute);
- 
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/message", messageRoute);
 
-server.listen(8080, ()=>{
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
     connectDB();
-    console.log(`Server listen at prot ${PORT}`);
+    console.log(`Server listening at port ${PORT}`);
 });
