d10e1f242c751e91d8061b861612424f44a29757
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-16 04:56:46 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'*',
+    origin:'https://mernchat-app-beryl.vercel.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
@@ -29,5 +29,5 @@ app.use("/api/v1/message",messageRoute);
 
 server.listen(8080, ()=>{
     connectDB();
-    console.log("Server listen at port 8080");
+    console.log("Server listen at port ${PORT}");
 });
