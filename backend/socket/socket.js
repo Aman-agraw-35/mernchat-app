48fb1762fdc2f4b321318cca063a566a8769dedd
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-17 01:18:09 +0530

    m

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,3 +1,7 @@
+import {Server} from "socket.io";
+import http from "http";
+import express from "express";
+const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
@@ -21,4 +25,3 @@ io.on('connection', (socket)=>{
     })
 })
 export {app, io, server};
-	
