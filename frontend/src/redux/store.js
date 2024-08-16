fb728b785581498f3be84036e322ca6046e45459
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-17 02:25:48 +0530

    ,,

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,40 +1,41 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice.js";
 import messageReducer from "./messageSlice.js";
 import socketReducer from "./socketSlice.js";
 import {
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-  } from 'redux-persist';
-  import storage from 'redux-persist/lib/storage'
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-  const persistConfig = {
-    key: 'root',
-    version: 1,
-    storage,
-  }
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  blacklist: ['socket'], // Prevents socket state from being persisted
+};
 
-  const rootReducer = combineReducers({
-    user:userReducer,
-    message:messageReducer,
-    socket:socketReducer
- })
+const rootReducer = combineReducers({
+  user: userReducer,
+  message: messageReducer,
+  socket: socketReducer,
+});
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-    reducer:persistedReducer,
-    middleware: (getDefaultMiddleware) =>
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
 });
+
 export default store;
