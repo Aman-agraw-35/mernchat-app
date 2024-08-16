2eb159ee9e7967bdaa3f00920b5388771dcb58c6
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-16 05:56:11 +0530

    ..

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -6,5 +6,11 @@ const router = express.Router();
 
 router.route("/send/:id").post(isAuthenticated,sendMessage);
 router.route("/:id").get(isAuthenticated, getMessage);
-
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+     });
 export default router;
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -8,5 +8,11 @@ router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/").get(isAuthenticated,getOtherUsers);
-
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+     });
 export default router;
