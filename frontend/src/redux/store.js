2aef04e60ef98d68226c474203164ef2fd4ee1e6
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-17 02:37:15 +0530

    .

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -10,14 +10,28 @@ import {
   PERSIST,
   PURGE,
   REGISTER,
+  createTransform
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+
+// Transform to exclude the socket state
+const removeSocketTransform = createTransform(
+  // transform state coming from redux on its way to being serialized and persisted.
+  (inboundState, key) => {
+    if (key === 'socket') return undefined; // Do not persist the socket state
+    return inboundState;
+  },
+  // transform state coming from storage, on its way to be rehydrated into redux
+  (outboundState, key) => {
+    return outboundState;
+  }
+);
 
 const persistConfig = {
   key: 'root',
   version: 1,
   storage,
-  blacklist: ['socket'], // Prevents socket state from being persisted
+  transforms: [removeSocketTransform], // Apply the transform
 };
 
 const rootReducer = combineReducers({
