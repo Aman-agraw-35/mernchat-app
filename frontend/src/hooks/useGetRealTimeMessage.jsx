2a10ce2df68f6895bc6f6b57feaa9098eb33e63e
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-17 03:11:05 +0530

    m

--- frontend/src/hooks/useGetRealTimeMessage.jsx
+++ frontend/src/hooks/useGetRealTimeMessage.jsx
@@ -1,16 +1,28 @@
-import { useEffect } from "react";
-import {useSelector, useDispatch} from "react-redux";
-import { setMessages } from "../redux/messageSlice";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { setMessages } from '../redux/messageSlice';
 
 const useGetRealTimeMessage = () => {
-    const {socket} = useSelector(store=>store.socket);
-    const {messages} = useSelector(store=>store.message);
+    const { socket } = useSelector(store => store.socket);
+    const { selectedUser, messages } = useSelector(store => store.user);
     const dispatch = useDispatch();
-    useEffect(()=>{
-        socket?.on("newMessage", (newMessage)=>{
-            dispatch(setMessages([...messages, newMessage]));
-        });
-        return () => socket?.off("newMessage");
-    },[setMessages, messages]);
+
+    useEffect(() => {
+        const handleNewMessage = (newMessage) => {
+            // Ensure the message is only added to the currently selected user's messages
+            if (newMessage.receiverId === selectedUser?._id) {
+                dispatch(setMessages(prevMessages => [...prevMessages, newMessage]));
+            }
+        };
+
+        socket?.on("newMessage", handleNewMessage);
+
+        return () => {
+            socket?.off("newMessage", handleNewMessage);
+        };
+    }, [socket, dispatch, selectedUser]);
+
+    // This hook does not return anything; it just manages real-time message updates
 };
+
 export default useGetRealTimeMessage;
