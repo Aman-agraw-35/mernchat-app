bd0a91f2eddb6588360f416b210b0e2f9b0392d3
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-17 02:55:19 +0530

    .

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import Message from './Message'
+import React from 'react';
+import Message from './Message';
 import useGetMessages from '../hooks/useGetMessages';
 import { useSelector } from "react-redux";
 import useGetRealTimeMessage from '../hooks/useGetRealTimeMessage';
 
 const Messages = () => {
-    // useGetMessages();
+    useGetMessages();
     useGetRealTimeMessage();
+    const { selectedUser } = useSelector(store => store.user);
     const { messages } = useSelector(store => store.message);
+
+    // Filter messages for the selected user
+    const filteredMessages = messages.filter(message => message.receiverId === selectedUser?._id);
+
     return (
         <div className='px-4 flex-1 overflow-auto'>
             {
-               messages && messages?.map((message) => {
-                    return (
-                        <Message key={message._id} message={message} />
-                    )
-                })
+                filteredMessages.map((message) => (
+                    <Message key={message._id} message={message} />
+                ))
             }
-
         </div>
+    );
+};
 
-
-    )
-}
-
-export default Messages
+export default Messages;
