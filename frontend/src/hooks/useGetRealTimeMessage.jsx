79304805afe42efb9943b854141e9383692d90ea
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-17 03:06:10 +0530

    m

--- frontend/src/hooks/useGetRealTimeMessage.jsx
+++ frontend/src/hooks/useGetRealTimeMessage.jsx
@@ -1,28 +1,16 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from "react-redux";
-import { setMessages } from '../redux/messageSlice';
+import { useEffect } from "react";
+import {useSelector, useDispatch} from "react-redux";
+import { setMessages } from "../redux/messageSlice";
 
 const useGetRealTimeMessage = () => {
-    const { socket } = useSelector(store => store.socket);
-    const { selectedUser, messages } = useSelector(store => store.user);
+    const {socket} = useSelector(store=>store.socket);
+    const {messages} = useSelector(store=>store.message);
     const dispatch = useDispatch();
-
-    useEffect(() => {
-        const handleNewMessage = (newMessage) => {
-            // Ensure the message is only added to the currently selected user's messages
-            if (newMessage.receiverId === selectedUser?._id) {
-                dispatch(setMessages(prevMessages => [...prevMessages, newMessage]));
-            }
-        };
-
-        socket?.on("newMessage", handleNewMessage);
-
-        return () => {
-            socket?.off("newMessage", handleNewMessage);
-        };
-    }, [socket, dispatch, selectedUser]);
-
-    // This hook does not return anything; it just manages real-time message updates
+    useEffect(()=>{
+        socket?.on("newMessage", (newMessage)=>{
+            dispatch(setMessages([...messages, newMessage]));
+        });
+        return () => socket?.off("newMessage");
+    },[setMessages, messages]);
 };
-
 export default useGetRealTimeMessage;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,55 +1,40 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import userReducer from "./userSlice.js";
 import messageReducer from "./messageSlice.js";
 import socketReducer from "./socketSlice.js";
 import {
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-  createTransform
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist';
+  import storage from 'redux-persist/lib/storage'
 
-// Transform to exclude the socket state
-const removeSocketTransform = createTransform(
-  // transform state coming from redux on its way to being serialized and persisted.
-  (inboundState, key) => {
-    if (key === 'socket') return undefined; // Do not persist the socket state
-    return inboundState;
-  },
-  // transform state coming from storage, on its way to be rehydrated into redux
-  (outboundState, key) => {
-    return outboundState;
+  const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
   }
-);
 
-const persistConfig = {
-  key: 'root',
-  version: 1,
-  storage,
-  transforms: [removeSocketTransform], // Apply the transform
-};
+  const rootReducer = combineReducers({
+    user:userReducer,
+    message:messageReducer,
+    socket:socketReducer
+ })
 
-const rootReducer = combineReducers({
-  user: userReducer,
-  message: messageReducer,
-  socket: socketReducer,
-});
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
+    reducer:persistedReducer,
+    middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
 });
-
 export default store;
