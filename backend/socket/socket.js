1e4edd6701846d6e7028edd29228f38dc78793df
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-21 01:23:49 +0530

    ,

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,27 +1,44 @@
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors:{
-        origin:['https://mernchat-app-pr7h.onrender.com/'],
-        methods:['GET', 'POST'],
+    cors: {
+        origin: ['https://mernchat-app-pr7h.onrender.com'],
+        methods: ['GET', 'POST'],
     },
 });
-export const getReceiverSocketId = (receiverId) => {
-    return userSocketMap[receiverId];
-}
-const userSocketMap = {}; // {userId->socketId}
-io.on('connection', (socket)=>{
-    const userId = socket.handshake.query.userId
-    if(userId !== undefined){
+
+const userSocketMap = {}; // {userId -> socketId}
+
+io.on('connection', (socket) => {
+    const userId = socket.handshake.query.userId;
+
+    // Map the user's ID to their socket ID
+    if (userId) {
         userSocketMap[userId] = socket.id;
-    } 
-    io.emit('getOnlineUsers',Object.keys(userSocketMap));
-    socket.on('disconnect', ()=>{
+    }
+
+    // Notify all clients about the updated list of online users
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
+
+    // Handle message sending
+    socket.on('sendMessage', ({ receiverId, message }) => {
+        const receiverSocketId = userSocketMap[receiverId];
+
+        // Send the message only to the intended recipient
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit('receiveMessage', { message, senderId: userId });
+        }
+    });
+
+    // Handle disconnection
+    socket.on('disconnect', () => {
         delete userSocketMap[userId];
-        io.emit('getOnlineUsers',Object.keys(userSocketMap));
-    })
-})
-export {app, io, server};
+        io.emit('getOnlineUsers', Object.keys(userSocketMap));
+    });
+});
+
+export { app, io, server };
