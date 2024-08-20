f20cfadf8e9ccaa489ba7b1a2726884664d7215f
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-21 00:47:01 +0530

    ..

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); 
 
 const connectDB = async () => {
-    console.log(process.env.MONGO_URI)
-    await mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('MongoDB URI:', process.env.MONGO_URI); 
+    await mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
         console.log('Database connected');
-    }).catch((error)=>{
-        console.log(error);
     })
+    .catch((error) => {
+        console.log('Database connection error:', error);
+    });
 };
+
 export default connectDB;
