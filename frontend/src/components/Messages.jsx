bd164d2851e0b9725dee6ba621f985edecae1806
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-21 01:34:32 +0530

    z

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Message from './Message'
-import useGetMessages from '../hooks/useGetMessages';
+// import useGetMessages from '../hooks/useGetMessages';
 import { useSelector } from "react-redux";
 import useGetRealTimeMessage from '../hooks/useGetRealTimeMessage';
 
 const Messages = () => {
-    useGetMessages();
+    // useGetMessages();
     useGetRealTimeMessage();
     const { messages } = useSelector(store => store.message);
     return (
