f64e4bc2fa836423a5a0708cd0f21561ec836635
Aman Agrawal <aman.agraw.35@gmail.com>
2024-08-21 01:52:01 +0530

    ,

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,44 +1,37 @@
-import { Server } from "socket.io";
+import {Server} from "socket.io";
 import http from "http";
 import express from "express";
 
 const app = express();
+
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: ['https://mernchat-app-pr7h.onrender.com'],
-        methods: ['GET', 'POST'],
+    cors:{
+        origin:['https://mernchat-app-pr7h.onrender.com'],
+        methods:['GET', 'POST'],
     },
 });
 
-const userSocketMap = {}; // {userId -> socketId}
+export const getReceiverSocketId = (receiverId) => {
+    return userSocketMap[receiverId];
+}
 
-io.on('connection', (socket) => {
-    const userId = socket.handshake.query.userId;
+const userSocketMap = {}; // {userId->socketId}
 
-    // Map the user's ID to their socket ID
-    if (userId) {
+
+io.on('connection', (socket)=>{
+    const userId = socket.handshake.query.userId
+    if(userId !== undefined){
         userSocketMap[userId] = socket.id;
-    }
+    } 
 
-    // Notify all clients about the updated list of online users
-    io.emit('getOnlineUsers', Object.keys(userSocketMap));
+    io.emit('getOnlineUsers',Object.keys(userSocketMap));
 
-    // Handle message sending
-    socket.on('sendMessage', ({ receiverId, message }) => {
-        const receiverSocketId = userSocketMap[receiverId];
-
-        // Send the message only to the intended recipient
-        if (receiverSocketId) {
-            io.to(receiverSocketId).emit('receiveMessage', { message, senderId: userId });
-        }
-    });
-
-    // Handle disconnection
-    socket.on('disconnect', () => {
+    socket.on('disconnect', ()=>{
         delete userSocketMap[userId];
-        io.emit('getOnlineUsers', Object.keys(userSocketMap));
-    });
-});
+        io.emit('getOnlineUsers',Object.keys(userSocketMap));
+    })
 
-export { app, io, server };
+})
+
+export {app, io, server};
