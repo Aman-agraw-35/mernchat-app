b616199650a52d1d3bad02be4caf189a53d62ab4
Aman Agrawal <aman.agraw.35@gmail.com>
2024-09-21 19:05:26 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import path from "path" ;
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://mernchat-app-pr7h.onrender.com',
     credentials:true
 };
 app.use(cors(corsOption)); 
@@ -20,13 +20,13 @@ app.use(cors(corsOption));
 app.use("/api/v1/user",userRoute); 
 app.use("/api/v1/message",messageRoute);
 app.use("/api/v1",deleteRoute);
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
-// app.use(express.static(path.join(__dirname,"../frontend/build")));
+app.use(express.static(path.join(__dirname,"../frontend/build")));
 
-// app.get("*", (req,res)=>{
-//      res.sendFile(path.join(__dirname,"../frontend/build/index.html"));
-// })
+app.get("*", (req,res)=>{
+     res.sendFile(path.join(__dirname,"../frontend/build/index.html"));
+})
 
 server.listen(8080, ()=>{
     connectDB();
